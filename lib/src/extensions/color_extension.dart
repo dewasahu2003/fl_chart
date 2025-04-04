e84c90a20b2b587156e8b48da2adcec555150b6b
dewasahu2003 <dewasahu2003@gmail.com>
2025-04-05 03:05:29 +0530

    changed for personal Pro

--- lib/src/extensions/color_extension.dart
+++ lib/src/extensions/color_extension.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 extension ColorExtension on Color {
-  /// Convert the color to a darken color based on the [percent]
+  /// Convert the color to alpha darken color based on the [percent]
   Color darken([int percent = 40]) {
     assert(1 <= percent && percent <= 100);
     final value = 1 - percent / 100;
     return Color.fromARGB(
-      _floatToInt8(a),
-      (_floatToInt8(r) * value).round(),
-      (_floatToInt8(g) * value).round(),
-      (_floatToInt8(b) * value).round(),
+      _floatToInt8(alpha),
+      (_floatToInt8(red) * value).round(),
+      (_floatToInt8(green) * value).round(),
+      (_floatToInt8(blue) * value).round(),
     );
   }
 
